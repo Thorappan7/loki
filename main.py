8910e28c525a43a65a7f446ed3ab4762cb5572e2
Thorappan7 <97897980+Thorappan7@users.noreply.github.com>
2022-01-22 22:44:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = Client(
     "my first projrct",
     api_id=502966,
     api_hash="ba91c94ee88658b8702befa528544df3",
-    bot_token="5065608218:AAGqoRYcy6wZfczCvYiXtIZUCNt4MLzIXyI",
+    bot_token="5153914057:AAGnHPEtgNYctk-zgoluy1PatigpuN65WD0",
     plugins = {"root": "bot" },
     sleep_threshold=5
 )
